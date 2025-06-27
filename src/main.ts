bf22bd1ed8f7c0d1171212aced029e64e611a6b7
Eduardo Perez <a.eduardoperez.fp2019@gmail.com>
2025-06-27 02:25:14 -0400

    agregamos mas para el template

--- src/main.ts
+++ src/main.ts
@@ -24,18 +24,9 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1');
 
   const config = new DocumentBuilder()
-    .setTitle('Glamofy API')
-    .setDescription(
-      'API para una aplicación de conexión entre profesionales de la belleza (barberos, manicuristas, peluquer@s) y clientes. Permite crear perfiles profesionales, buscar servicios cercanos mediante geolocalización, agendar citas, recibir calificaciones y ofrecer servicios a domicilio o en local.',
-    )
+    .setTitle('Template title')
+    .setDescription('Template description')
     .setVersion('1.0')
-    .addTag('Autenticación')
-    .addTag('Usuarios')
-    .addTag('Profesionales')
-    .addTag('Servicios')
-    .addTag('Reservas')
-    .addTag('Ubicación')
-    .addTag('Reputación')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
